72fb29392f7f5fd7e2b8315312e92cfc0149be25
hasegawa_itsuki <ii440092@icloud.com>
2022-03-22 09:48:27 +0900

    namespaceを利用

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,16 @@
 <template>
-  <Tutorial />
+  <div>
+    <div>count:{{ this.$store.state.count.count }}</div>
+    <button @click="addCount">Add</button>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  methods: {
+    addCount () {
+      this.$store.dispatch("count/increment");
+    },
+  },
+};
 </script>
