957964c0230115a620cbc9e621c571cd74fe5a67
hasegawa_itsuki <ii440092@icloud.com>
2022-03-22 10:38:42 +0900

    Vueのプラグインを利用

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <div>
     <div>count:{{ this.$store.state.count.count }}</div>
     <button @click="addCount">Add</button>
+    <button v-tooltip="'TOPにメッセージが表示されます'">TOP</button>
   </div>
 </template>
 
@@ -14,3 +15,43 @@ export default {
   },
 };
 </script>
+
+<style>
+.tooltip {
+  display: block !important;
+  padding: 4px;
+  z-index: 10000;
+}
+
+.tooltip .tooltip-inner {
+  background: black;
+  color: white;
+  border-radius: 16px;
+  padding: 5px 10px 4px;
+}
+
+.tooltip .tooltip-arrow {
+  display: none;
+}
+
+.tooltip[aria-hidden="true"] {
+  visibility: hidden;
+  opacity: 0;
+  transition: opacity 0.15s, visibility 0.15s;
+}
+
+.tooltip[aria-hidden="false"] {
+  visibility: visible;
+  opacity: 1;
+  transition: opacity 0.15s;
+}
+
+.tooltip.danger .tooltip-inner {
+  background: red;
+}
+
+.tooltip.caution .tooltip-inner {
+  background: yellow;
+  color: black;
+}
+</style>
